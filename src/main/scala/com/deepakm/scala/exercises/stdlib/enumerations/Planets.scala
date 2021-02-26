d146d94b6cc248a11f633bbe62463c01f4c8e03b
deepak.marathe <dpkmarathe@gmail.com>
2021-02-26 15:39:40 +0530

    Enumerations

---
+++ src/main/scala/com/deepakm/scala/exercises/stdlib/enumerations/Planets.scala
@@ -0,0 +1,13 @@
+package com.deepakm.scala.exercises.stdlib.enumerations
+
+object Planets extends Enumeration {
+  val Mercury = Value
+  val Venus = Value
+  val Earth = Value
+  val Mars = Value
+  val Jupiter = Value
+  val Saturn = Value
+  val Uranus = Value
+  val Neptune = Value
+  val Pluto = Value
+}
